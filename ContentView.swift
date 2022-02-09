64fec36e6fb7feafc669c9f4f3c9d1ed8d29a34d
Zheng <aheze@getfind.app>
2022-02-09 11:43:16 -0800

    Base

--- ContentView.swift
+++ ContentView.swift
@@ -1,82 +1,50 @@
 import SwiftUI
 
-struct Container: ViewModifier {
-    let image: String
-
-    @State var showingImage = true
-    @State var showingResult = false
-    
-    func body(content: Content) -> some View {
-        HStack {
-            Button {
-                withAnimation {
-                    showingImage.toggle()
-                }
-            } label: {
-                LinearGradient(
-                    colors: [.blue, .green],
-                    startPoint: .topLeading,
-                    endPoint: .bottomTrailing
-                )
-                .opacity(showingImage ? 0 : 1)
-                .overlay(
-                    Image(image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .opacity(showingImage ? 1 : 0)
-                )
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color(uiColor: .systemBackground))
-                .cornerRadius(24)
-            }
-
-            Button {
-                withAnimation {
-                    showingResult.toggle()
-                }
-            } label: {
-                LinearGradient(
-                    colors: [.red, .yellow],
-                    startPoint: .topLeading,
-                    endPoint: .bottomTrailing
-                )
-                .opacity(showingResult ? 0 : 1)
-                .overlay(
-                    content
-                        .opacity(showingResult ? 1 : 0)
-                )
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color(uiColor: .systemBackground))
-                .cornerRadius(24)
-            }
-        }
-        .padding()
-        .background(Color(uiColor: .secondarySystemBackground))
-    }
-}
-
-extension View {
-    func addContainer(image: String) -> some View {
-        modifier(Container(image: image))
-    }
-}
-
 struct ContentView: View {
-    @State var selectedIndex = 1
+    @State var selectedIndex = 12
     var body: some View {
         VStack(spacing: 0) {
             VStack {
                 switch selectedIndex {
-                case 1: ContentView1().addContainer(image: "1")
-                case 2: ContentView2().addContainer(image: "2")
-                case 3: ContentView3().addContainer(image: "3")
-                case 4: ContentView4().addContainer(image: "4")
-                case 5: ContentView5().addContainer(image: "5")
-                case 6: ContentView6().addContainer(image: "6")
-                case 7: ContentView7().addContainer(image: "7")
-                case 8: ContentView8().addContainer(image: "8")
-                case 9: ContentView9().addContainer(image: "9")
-                    
+                case 1:
+                    Slide {
+                        Text("Views")
+                            .font(.system(size: 100))
+                            .foregroundColor(.blue)
+                    }
+                case 2:
+                    BasicView().scaleEffect(1.5).addContainer(image: "Basic", title: "Basic Views")
+                case 3:
+                    Slide {
+                        Text("Modifiers")
+                            .font(.system(size: 100))
+                            .foregroundColor(.purple)
+                    }
+                case 4: PaddingView().scaleEffect(1.5).addContainer(image: "Padding", title: ".padding")
+                case 5: BackgroundView().scaleEffect(1.5).addContainer(image: "Background", title: ".background")
+                case 6: FontView().scaleEffect(1.5).addContainer(image: "Font", title: ".font")
+                case 7: OpacityView().scaleEffect(1.5).addContainer(image: "Opacity", title: ".opacity")
+                case 8: ForegroundColorView().scaleEffect(1.5).addContainer(image: "ForegroundColor", title: ".foregroundColor")
+                case 9: CornerRadiusView().scaleEffect(1.5).addContainer(image: "Corner", title: ".cornerRadius")
+                case 10: FrameView().addContainer(image: "Frame", title: ".frame")
+                case 11:
+                    Text("Challenge")
+                        .font(.system(size: 200))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(
+                            LinearGradient(
+                                colors: [.blue, .green],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .cornerRadius(32)
+                        .padding(64)
+                case 12:
+                    ChallengeView()
+                        .frame(width: 500)
+                        .scaleEffect(2)
                 default: Color.gray
                 }
             }
@@ -85,7 +53,7 @@ struct ContentView: View {
             HStack {
                 Group {
                     Button {
-                        if selectedIndex >= 2 {
+                        if selectedIndex >= 1 {
                             selectedIndex -= 1
                         }
                     } label: {
@@ -101,9 +69,7 @@ struct ContentView: View {
                         .frame(maxWidth: .infinity)
 
                     Button {
-                        if selectedIndex <= 10 {
-                            selectedIndex += 1
-                        }
+                        selectedIndex += 1
                     } label: {
                         Image(systemName: "chevron.right")
                             .font(.largeTitle.bold())
--- Views.swift
+++ Views.swift
@@ -5,14 +5,173 @@
 //  Created by A. Zheng (github.com/aheze) on 1/26/22.
 //  Copyright © 2022 A. Zheng. All rights reserved.
 //
-    
 
 import SwiftUI
 
-struct ContentView1: View {
+struct BasicView: View {
     var body: some View {
-        Text("Hello, world!")
-            .scaleEffect(3)
+        VStack {
+            Text("Hello, world!")
+
+            HStack {
+                Image(systemName: "star.fill")
+                Text("This is a star")
+            }
+        }
+    }
+}
+
+struct PaddingView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .border(.blue)
+
+            Text("Hello, world!")
+                .padding(60)
+                .border(.blue)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+
+            Text("Hello, world!")
+                .background(Color.yellow)
+
+            Text("Hello, world!")
+                .background(
+                    HStack(spacing: 0) {
+                        Color.red
+                        Color.green
+                        Color.blue
+                    }
+                )
+        }
+    }
+}
+
+struct FontView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+
+            Text("Hello, world!")
+                .font(.largeTitle)
+        }
+    }
+}
+
+struct ForegroundColorView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+
+            Text("Hello, world!")
+                .foregroundColor(.green)
+        }
+    }
+}
+
+struct OpacityView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+
+            Text("Hello, world!")
+                .opacity(0.5)
+        }
+    }
+}
+
+struct CornerRadiusView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .background(Color.green)
+
+            Text("Hello, world!")
+                .background(Color.green)
+                .cornerRadius(8)
+        }
+    }
+}
+
+struct FrameView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .border(.red)
+
+            Text("Hello, world!")
+                .frame(width: 40, height: 30)
+                .border(.red)
+
+            Text("Hello, world!")
+                .frame(maxWidth: .infinity)
+                .border(.red)
+        }
+    }
+}
+
+struct ChallengeView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Coding Club")
+                Image(systemName: "star.fill")
+                Text("Group 1")
+            }
+            .foregroundColor(.blue)
+            .padding(24)
+            .frame(maxWidth: .infinity)
+            .background(Color.blue.opacity(0.2))
+            .background(Color.white)
+            
+
+            VStack {
+                HStack {
+                    Text("Question:")
+                        .bold()
+                    Text("Why did the chicken cross the road?")
+                }
+
+                HStack {
+                    Text("Answer")
+                        .bold()
+                    Text("TODO")
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.white.opacity(0.3))
+            .cornerRadius(16)
+            .padding()
+
+            VStack {
+                Text("Members:")
+                    .bold()
+
+                Text("Andrew")
+                Text("Harris")
+                Text("Clement")
+                Text("Krishna")
+                Text("Tyler")
+                Text("Isaac")
+                Text("Ariel")
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.white.opacity(0.3))
+            .cornerRadius(16)
+            .padding()
+        }
+        .foregroundColor(.white)
+        .background(.blue)
+        .cornerRadius(16)
     }
 }
 
@@ -105,12 +264,12 @@ struct ContentView8: View {
                 .background(Color.green)
                 .cornerRadius(12)
                 .foregroundColor(.white)
-            
+
             VStack {
                 Text("Hello!")
                     .font(Font.system(size: 15))
                     .foregroundColor(Color.black)
-                    
+
                 Text("My name is _______")
                     .font(Font.system(size: 15))
                     .foregroundColor(Color.black)
@@ -124,14 +283,14 @@ struct ContentView9: View {
     var body: some View {
         VStack {
             Text("Choose.")
-            
+
             HStack {
                 Image(systemName: "person")
                     .padding(10)
                     .foregroundColor(Color.white)
                     .background(Color.blue)
                     .cornerRadius(12)
-                
+
                 Image(systemName: "dice")
                     .padding(10)
                     .foregroundColor(Color.white)
@@ -142,4 +301,3 @@ struct ContentView9: View {
         .scaleEffect(3)
     }
 }
-
